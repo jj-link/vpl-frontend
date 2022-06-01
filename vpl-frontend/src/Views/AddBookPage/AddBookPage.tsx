903974c5e357258950b9c1b10c54d3014cb29086
joseph link <joseph.j.link@gmail.com>
2022-05-31 23:13:22 -0400

    added addBook

---
+++ vpl-frontend/src/Views/AddBookPage/AddBookPage.tsx
@@ -0,0 +1,29 @@
+import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../Store';
+import { useNavigate } from 'react-router-dom';
+
+import { AddBookForm } from '../../Components/AddBookForm/AddBookForm';
+
+
+// will go inside App tsx
+export const AddBookPage: React.FC = () => {
+    const ownerInfo = useSelector((state: RootState) => state.user);
+    const navigator = useNavigate();
+
+    useEffect(() => {
+        if (!ownerInfo.isLoggedIn || ownerInfo.user?.userRole != 2) {
+          console.log("this ran and went back to login");
+          navigator('/login');
+        }
+        else{
+          //console.log(userInfo);
+        }
+      }, [ownerInfo.isLoggedIn, ownerInfo.user?.userRole]);
+
+  return (
+    <div className="add-book-page">
+      <AddBookForm/>
+    </div>
+  );
+};
