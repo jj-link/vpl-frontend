368fad29bad01b2e5e594c363b11a589d4a381f5
S1m1ster <darrylrobles14@gmail.com>
2022-06-04 14:59:44 -0400

    more design

--- vpl-frontend/src/Views/GenreBookListPage/GenreBookListPage.tsx
+++ vpl-frontend/src/Views/GenreBookListPage/GenreBookListPage.tsx
@@ -1,6 +1,6 @@
 
 import React, { useEffect } from 'react';
-//import './GenreBookListPage.css';
+import './GenreBookListPage.css';
 import { Link, useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { AppDispatch, RootState } from '../../Store'
@@ -35,30 +35,30 @@ export const GenreBookListPage: React.FC<any> = () => {
   return (
     <div className='genre-list-books'>
         <Navbar />
-        <div className="page-title">
+        <div className="genre-list-page-title">
             <h3>Books by Genre: </h3>
         </div>
 
         {bookInfo.genrebooklist?.map((book) => {
           return (
-            <div className = 'book-container'>
-              <div className='book-details' key={book.bookId}>
+            <div className = 'genre-list-book-container'>
+              <div className='genre-list-book-details' key={book.bookId}>
                 <p>Title: {book.title}</p>
                 <p>Author: {book.author}</p>
                 <p>GenreId: {book.genreId}</p>
                 <p>ISBN: {book.isbn}</p>
                 <p>Summay: {book.summary}</p>
               </div>
-              <div className='checkout-button'>
-                  <button data-id={book.isbn} onClick={handleCheckout}>Checkout</button>
+              <div className='genre-list-checkout-button'>
+                  <button className='genre-list-checkout-btn' data-id={book.isbn} onClick={handleCheckout}>Checkout</button>
               </div>
             </div>
           );
         })}
 
-        <div className="home-button">
+        <div className="genre-list-home-button">
           <Link to="/home">
-            <button>back</button>
+            <button className='genre-list-home-btn'>back</button>
           </Link>
         </div>
     </div>
