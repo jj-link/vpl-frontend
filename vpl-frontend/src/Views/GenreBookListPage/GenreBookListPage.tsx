a81e2bac55be3a7477342c4f774415324eff1e0c
joseph link <joseph.j.link@gmail.com>
2022-06-03 21:34:50 -0400

    added checkout buttons

--- vpl-frontend/src/Views/GenreBookListPage/GenreBookListPage.tsx
+++ vpl-frontend/src/Views/GenreBookListPage/GenreBookListPage.tsx
@@ -1,16 +1,30 @@
 
 import React, { useEffect } from 'react';
 //import './GenreBookListPage.css';
-import { Link } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import { RootState } from '../../Store'
+import { Link, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { AppDispatch, RootState } from '../../Store'
 import { Navbar } from '../../Components/Navbar/Navbar';
+import { checkoutBook } from '../../Slices/BookSlice';
 
 // go inside App for routing
 export const GenreBookListPage: React.FC<any> = () => {
   const bookInfo = useSelector((state: RootState) => state.book);
-  // const navigator = useNavigate();
-  // const dispatch: AppDispatch = useDispatch();
+  const userInfo = useSelector((state: RootState) => state.user)
+  const navigator = useNavigate();
+  const dispatch: AppDispatch = useDispatch();
+
+  const handleCheckout=(event: React.MouseEvent<HTMLButtonElement>) =>{
+
+    let credentials = {
+      userId: userInfo.user?.userId,
+      isbn: 0
+    }
+
+    credentials.isbn = parseInt(event.currentTarget.getAttribute("data-id")!);
+    dispatch(checkoutBook(credentials));
+    navigator('/userhome');
+  }
 
   useEffect(() =>{
     console.log("in useeffect");
@@ -34,6 +48,9 @@ export const GenreBookListPage: React.FC<any> = () => {
                 <p>GenreId: {book.genreId}</p>
                 <p>ISBN: {book.isbn}</p>
                 <p>Summay: {book.summary}</p>
+              </div>
+              <div className='checkout-button'>
+                  <button data-id={book.isbn} onClick={handleCheckout}>Checkout</button>
               </div>
             </div>
           );
--- vpl-frontend/src/Views/RecentBooksPage/RecentBooksPage.tsx
+++ vpl-frontend/src/Views/RecentBooksPage/RecentBooksPage.tsx
@@ -1,15 +1,29 @@
 import React from 'react';
 //import './RecentBooksPage.css';
-import { Link } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import { RootState } from '../../Store'
+import { Link, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { AppDispatch, RootState } from '../../Store'
 import { Navbar } from '../../Components/Navbar/Navbar';
+import { checkoutBook } from '../../Slices/BookSlice';
 
 // go inside App for routing
 export const RecentBooksPage: React.FC<any> = () => {
   const bookInfo = useSelector((state: RootState) => state.book);
-  // const navigator = useNavigate();
-  // const dispatch: AppDispatch = useDispatch();
+  const userInfo = useSelector((state: RootState) => state.user)
+  const navigator = useNavigate();
+  const dispatch: AppDispatch = useDispatch();
+
+  const handleCheckout=(event: React.MouseEvent<HTMLButtonElement>) =>{
+
+    let credentials = {
+      userId: userInfo.user?.userId,
+      isbn: 0
+    }
+
+    credentials.isbn = parseInt(event.currentTarget.getAttribute("data-id")!);
+    dispatch(checkoutBook(credentials));
+    navigator('/userhome');
+  }
 
   return (
     <div className='recent-books'>
@@ -27,6 +41,9 @@ export const RecentBooksPage: React.FC<any> = () => {
                 <p>GenreId: {book.genreId}</p>
                 <p>ISBN: {book.isbn}</p>
                 <p>Summay: {book.summary}</p>
+              </div>
+              <div className='checkout-button'>
+                  <button data-id={book.isbn} onClick={handleCheckout}>Checkout</button>
               </div>
             </div>
           );
